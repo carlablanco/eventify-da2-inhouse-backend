3a3317a959b54309a0a775cb545cfd325c65a3db
snicolino <70871682+snicolino@users.noreply.github.com>
2024-09-20 19:54:50 -0300

    add test user controller (#4)
    Co-authored-by: Carla Blanco <88563106+carlablanco@users.noreply.github.com>

---
+++ src/__tests__/userController.test.js
@@ -0,0 +1,55 @@
+const { getUserById } = require('../controllers/user.controller'); 
+const UserService = require('../service/user.service'); 
+const diccionarioMensajes = require('../utils/diccionarioMensajes'); 
+
+jest.mock('../service/user.service'); // Mockeamos UserService
+
+describe('getUserById', () => {
+    let req, res;
+
+    beforeEach(() => {
+        req = { query: {} };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+    });
+
+    it('should return 400 if no id is provided', async () => {
+        await getUserById(req, res);
+        expect(res.status).toHaveBeenCalledWith(diccionarioMensajes[10].statusHttp);
+        expect(res.json).toHaveBeenCalledWith({
+            status: diccionarioMensajes[10].statusHttp,
+            message: diccionarioMensajes[10].message,
+        });
+    });
+
+    it('should return user data if id is provided', async () => {
+        req.query.id = '1';
+        const mockUser = { id: '1', name: 'John Doe' }; // Mock de usuario
+
+        UserService.getUserById.mockResolvedValue(mockUser); // Mockeamos el retorno
+
+        await getUserById(req, res);
+        expect(res.status).toHaveBeenCalledWith(diccionarioMensajes[0].statusHttp);
+        expect(res.json).toHaveBeenCalledWith({
+            status: diccionarioMensajes[0].statusHttp,
+            data: mockUser,
+            message: diccionarioMensajes[0].message,
+        });
+    });
+
+    it('should handle errors', async () => {
+        req.query.id = '1';
+        const mockError = { statusHttp: 500, message: 'Internal Server Error' };
+
+        UserService.getUserById.mockRejectedValue(mockError); // Simulamos un error
+
+        await getUserById(req, res);
+        expect(res.status).toHaveBeenCalledWith(mockError.statusHttp);
+        expect(res.json).toHaveBeenCalledWith({
+            status: mockError.statusHttp,
+            message: mockError.message,
+        });
+    });
+});
