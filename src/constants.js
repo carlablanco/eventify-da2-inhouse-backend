6cd3fc400a83d9daa4ef68bcb44b407394d8054c
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2024-09-21 14:10:32 -0300

    Modifiacion menor

--- src/constants.js
+++ src/constants.js
@@ -6,15 +6,13 @@ const NODE_ENV = 'development'
 const SECRET_KEY_JWT = 'd9e7ff74c8d7c3ff41b4a3636a7645e1f100e4c9'
 const REFRESH_TOKEN_SIZE = 32
 const TOKEN_EXPIRATION = 3600000*168 // Seteo en un ahora la validez del access_token
+
+// Seccion para conexion a base de datos
 const MONGO_USER = ''
 const MONGO_PASS = ''
 const MONGO_DB = ''
 const MONGO_CLUSTER = ''
 const MONGO_APP_NAME = ''
-
-
-// Seccion para conexion a base de datos
-
 
 module.exports = {
     PORT, 
