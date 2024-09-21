004670bde6501027de96b2b7ef6109dbd98d2b5c
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2024-09-21 13:22:30 -0300

    Se agregan nuevas cosntante para conexion a mongo

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const PORT = 3500
+const PORT = 3000
 const NODE_ENV = 'development'
 // const NODE_ENV=production
 
@@ -6,6 +6,12 @@ const NODE_ENV = 'development'
 const SECRET_KEY_JWT = 'd9e7ff74c8d7c3ff41b4a3636a7645e1f100e4c9'
 const REFRESH_TOKEN_SIZE = 32
 const TOKEN_EXPIRATION = 3600000*168 // Seteo en un ahora la validez del access_token
+const MONGO_USER = ''
+const MONGO_PASS = ''
+const MONGO_DB = ''
+const MONGO_CLUSTER = ''
+const MONGO_APP_NAME = ''
+
 
 // Seccion para conexion a base de datos
 
@@ -15,5 +21,10 @@ module.exports = {
     NODE_ENV, 
     SECRET_KEY_JWT, 
     REFRESH_TOKEN_SIZE, 
-    TOKEN_EXPIRATION
+    TOKEN_EXPIRATION,
+    MONGO_USER, 
+    MONGO_PASS, 
+    MONGO_CLUSTER, 
+    MONGO_APP_NAME,
+    MONGO_DB
 }
