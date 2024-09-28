b03a2f676c16813b59ac1210a30ec434d8ec5816
Sebastian Nicolino <sebastian.o.nicolino@gmail.com>
2024-09-28 17:04:33 -0300

    agrego login con LDAP, creo roles

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,55 +1,9 @@
-// // Gettign the Newly created Mongoose Model we just created
-// var LdapClient = require('ldapjs-client');
-// var client = new LdapClient({ url: 'ldap://10.8.0.1:389' });
-//
-// const diccionarioMensajes = require('../utils/requestMessages');
-//
-// // Saving the context of this module inside the _the variable
-// _this = this
-//
-// exports.getUserById = async function (idUser) {
-//
-//     try {
-//         const options = {
-//           filter: `(&(cn=${idUser}))`,
-//           scope: 'sub',
-//           attributes: ['sn', 'cn']
-//         };
-//
-//         const entries = await client.search('dc=eventify,dc=local', options);
-//
-//         console.log(entries)
-//         return entries;
-//
-//       } catch (e) {
-//         console.log(e);
-//       }
-//
-// }
-//
-// exports.login = async function (cn, pass) {
-//
-//     try {
-//
-//         var userDn = `cn=${cn}` + ",ou=eventify_sa,dc=eventify,dc=local"
-//         const login = await client.bind( userDn , pass);
-//
-//         console.log(login)
-//         return login;
-//
-//       } catch (e) {
-//         console.log(e);
-//       }
-//
-// }
-//
-//
-//
-//
-//
-
 const UserModel = require("../models/Users");
 const bcrypt = require("bcrypt");
+const { LDAP_IP, LDAP_DC } = process.env;
+var LdapClient = require('ldapjs-client');
+var client = new LdapClient({ url: LDAP_IP });
+
 
 class UserService {
   async getUsers() {
@@ -77,6 +31,26 @@ class UserService {
       console.error(err);
       throw new Error("Error in getUserById Service");
     }
+  }
+
+  async ldapGetUserByEmail (email) {
+    
+    try {
+        const options = {
+          filter: `(&(cn=${email}))`,
+          scope: 'sub',
+          attributes: ['sn', 'cn', 'ou', 'telephoneNumber']
+        };
+
+        const entries = await client.search(`dc=${LDAP_DC}`, options);
+
+        return entries[0];
+
+      } catch (err) {
+        console.log(err);
+        throw new Error("Error in ldapGetUserById Service");
+      }
+
   }
 
   async createUser(user) {
