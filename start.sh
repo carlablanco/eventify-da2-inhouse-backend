e7d4d4152aae60cba1186b02f9c0c2e27cc51856
carlablanco <carla@MacBook-Air-de-Carla.local>
2024-10-23 19:20:13 -0300

    path relatvivo gunicorn

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 npm start &
 
 # Gunicorn con flask para python
-python3 -m gunicorn -w 4 -b 0.0.0.0:8000 webapp_flask:app
+python3 -m gunicorn -w 4 -b 0.0.0.0:8000 machine_learning.webapp_flask:app
