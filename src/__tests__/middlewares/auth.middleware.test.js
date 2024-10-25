8c67d840a9e8baf9dbd1aebe4f2fdc5cb6afef2d
Nicolás Facón <nicofacon93@gmail.com>
2024-10-25 12:45:33 -0300

    Genera tests de auth.middleware.

--- src/__tests__/middlewares/auth.middleware.test.js
+++ src/__tests__/middlewares/auth.middleware.test.js
@@ -3,6 +3,9 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const validateJwt = require('../../middlewares/auth.middleware');
 
+// Habilito o deshabilito los console.error para mostrarse en la consola durante el test.
+const SHOW_CONSOLE_LOGS = false;
+const SHOW_CONSOLE_ERRORS = false;
 
 // Crear una aplicación Express para las pruebas
 const app = express();
@@ -20,7 +23,6 @@ process.env.NODE_ENV = 'test'; // Cambiamos a un entorno de prueba
 jest.mock('jsonwebtoken', () => ({
   verify: jest.fn(),
 }));
-
 
 /*  it('should return 401 for invalid JWT', async () => {
    const { verify } = require('jsonwebtoken');
@@ -49,6 +51,40 @@ jest.mock('jsonwebtoken', () => ({
    expect(response.status).toBe(401);
    expect(response.body).toEqual({ message: 'Usuario no autorizado' });
  }); */
-test('dummy test', async () => {
-  expect(true);
-})
+describe('AuthMiddleware', () => {
+  let req, res;
+
+  beforeEach(() => {
+    if (!SHOW_CONSOLE_ERRORS)
+      // Silencia console.error
+      jest.spyOn(console, 'error').mockImplementation(() => { });
+
+    if (!SHOW_CONSOLE_LOGS)
+      // Silencia console.log
+      jest.spyOn(console, 'log').mockImplementation(() => { });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+
+    if (!SHOW_CONSOLE_ERRORS)
+      // Restaura console.error
+      console.error.mockRestore();
+
+    if (!SHOW_CONSOLE_LOGS)
+      // Restaura console.log
+      console.log.mockRestore();
+  });
+
+  test('validateJwt debería llamar al callback', async () => {
+    // Crea una función mock para el callback
+    const mockCallback = jest.fn();
+
+    // Llamamos al método validateJwt
+    await validateJwt(req, res, mockCallback);
+
+    // Expectativas
+    // Verifica si el callback fue llamado
+    expect(mockCallback).toHaveBeenCalled();
+  });
+});
